0d86e251f78eb1f7186fa641ddd3b05447f0693b
colin-yard-nl <colin-yard-nl@users.noreply.github.com>
2024-08-22 12:22:05 +0000

    (auto): apply php-cs-fixer changes

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,12 +11,12 @@ it('can retrieve a random inspirational quote', function () {
 it('can retrieve post content', function () {
     $postId = 123;
     $post = new stdClass();
-        $post->post_content = 'Hello World!';
+    $post->post_content = 'Hello World!';
 
     WP_Mock::userFunction('get_post')
-            ->once()
-            ->with(123)
-            ->andReturn($post);
+        ->once()
+        ->with(123)
+        ->andReturn($post);
 
     $postContent = app()->make('Example')->getPostContent($postId);
 
